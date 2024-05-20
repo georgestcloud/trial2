041e45a06de6ae93249dbd37fc8d1cea3a27ec67
George Chinaka <132566100+georgestcloud@users.noreply.github.com>
2024-05-20 06:08:06 +0100

    script.js

--- script.js
+++ script.js
@@ -152,3 +152,21 @@ closeButtons2.forEach(button => {
 
 
 
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
