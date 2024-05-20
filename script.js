657506a87654d72935ff7ad025c2139ebcbaf0dc
George Chinaka <132566100+georgestcloud@users.noreply.github.com>
2024-05-20 08:30:58 +0100

    script.js

--- script.js
+++ script.js
@@ -131,31 +131,28 @@ window.onresize = function() {
 
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("openModalBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
+  const modal = document.getElementById("myModal");
+  const openModalBtn = document.getElementById("openModal");
+  const closeModalBtn = document.getElementById("closeModal");
+  const closeModalFooterBtn = document.getElementById("closeModalFooter");
+
+  openModalBtn.onclick = function() {
     modal.style.display = "block";
-    document.body.style.overflow = "hidden"; // Disable background scrolling
-}
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+  closeModalBtn.onclick = function() {
     modal.style.display = "none";
-    document.body.style.overflow = "auto"; // Re-enable background scrolling
-}
+  }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+  closeModalFooterBtn.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
-        document.body.style.overflow = "auto"; // Re-enable background scrolling
+      modal.style.display = "none";
     }
-}
+  }
