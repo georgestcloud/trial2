71ac0745f6c0fd9a24b991f80ef2ee2e5855c12c
George Chinaka <132566100+georgestcloud@users.noreply.github.com>
2024-05-20 08:03:29 +0100

    script.js

--- script.js
+++ script.js
@@ -124,13 +124,21 @@ window.onresize = function() {
 
 
 
+
+
+
+
+
+
+
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("continueBtn");
 
-// Get the <span> element that closes the modal
+// Get the <span> elements that close the modal
 var spans = document.getElementsByClassName("close");
 
 // When the user clicks the button, open the modal
@@ -138,14 +146,14 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x) or "Close", close the modal
 for (var i = 0; i < spans.length; i++) {
   spans[i].onclick = function() {
     modal.style.display = "none";
   }
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal content, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
